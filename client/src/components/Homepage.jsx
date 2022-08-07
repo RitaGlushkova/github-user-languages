563ff95737fd66812f3a2eb95877688a117d9992
Margarita Glushkova <m.glushkova1@gmail.com>
2022-08-07 23:16:40 +0100

    functionality added
    Added functionality based on 100 repos,
    counted per number of repos written in a language not bites

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState, useRef } from "react";
+import { useState } from "react";
 import axios from "axios";
 import Header from "./moledules/Header";
 import { Input } from "./atoms/Input";
@@ -8,51 +8,27 @@ import { Button } from "./atoms/Button";
 const Homepage = () => {
   const [username, setUsername] = useState("");
   const [language, setLanguage] = useState("");
-  //const inputRef = useRef(null);
 
-  // const handleClick = () => {
-  //   inputRef.current.focus();
-  //   setUsername(inputRef.current.value);
-  //   if (username.length) getResults(username);
-  // };
   const handleClick = () => {
-    getResults(username)
-  }
+    getResults(username);
+  };
 
   const getResults = (input) => {
-    let endPoints = [];
     axios({
       headers: {
         Authorization: `token ${process.env.REACT_APP_GH}`,
       },
       method: "get",
-      url: `https://api.github.com/users/${input}/repos?per_page=10`,
-    })
-      .then((response) => {
-        console.log(response.data)
-        endPoints = response.data.map((el) => el.languages_url);
-      })
-      .then(() =>
-        axios
-          .all(
-            endPoints.map((endpoint) =>
-              axios({
-                headers: {
-                  Authorization: `token ${process.env.REACT_APP_GH}`,
-                },
-                method: "get",
-                url: endpoint,
-              })
-            )
-          )
-          .then((result) => {
-            const arrayOfAllLanguagesForEachRepo = result.map((el) => el.data);
-            const winner = Object.entries(
-              findMostUsedLanguageByBites(arrayOfAllLanguagesForEachRepo)
-            )[0];
-            setLanguage(`${winner[0]}: ${winner[1]}%`);
-          })
-      );
+      url: `https://api.github.com/users/${input}/repos?per_page=100`,
+    }).then((response) => {
+      const languagesArray = response.data
+        .map((el) => el.language)
+        .filter((v) => v);
+      const winner = Object.entries(
+        findMostUsedLanguageByBites(languagesArray)
+      )[0];
+      setLanguage(`${winner[0]}: ${winner[1]}%`);
+    });
   };
   const handleReset = () => {
     setUsername("");
@@ -66,7 +42,6 @@ const Homepage = () => {
         value={username}
         label="GitHub Username"
         placeholderText="write GitHub Username"
-        //forwardRef={inputRef}
         onChange={(e) => setUsername(e.target.value)}
       />
       <Button btnName="Get Result" handleClick={handleClick} />
@@ -81,17 +56,15 @@ const Homepage = () => {
 };
 
 const findMostUsedLanguageByBites = (array) => {
-  const object = {};
   let language = {};
-  array.forEach((repoLanguages) => {
-    Object.keys(repoLanguages).forEach((l) => {
-      if (l in object) object[l] = object[l] + repoLanguages[l];
-      else object[l] = repoLanguages[l];
-    });
+  let object = {};
+  array.forEach((l) => {
+    if (l in object) object[l] = object[l] + 1;
+    else object[l] = 1;
   });
-  const totalSumOfBites = Object.values(object).reduce((a, b) => a + b, 0);
+  const total = Object.values(object).reduce((a, b) => a + b, 0);
   for (let i in object) {
-    object[i] = Math.floor((object[i] / totalSumOfBites) * 100);
+    object[i] = Math.floor((object[i] / total) * 100);
   }
   for (let i in object) {
     if (object[i] === Math.max(...Object.values(object)))
